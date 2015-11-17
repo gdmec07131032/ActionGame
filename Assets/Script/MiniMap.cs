8930ebb936d1d5e25b45da7334ab3d9d51212c45
gdmec07131032 <592876487@qq.com>
2015-11-17 21:24:01 +0800

    2015/11/17-21：23

---
+++ Assets/Script/MiniMap.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMap : MonoBehaviour {
+    public static MiniMap _instance;
+    private Transform playerIcon;
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+	// Use this for initialization
+	void Start () {
+        playerIcon = transform.Find("PlayerIcon");
+	}
+
+    public Transform GetPlayerIcon()
+    {
+        return playerIcon;
+    }
+}
---
+++ Assets/Script/PlayerIcon.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerIcon : MonoBehaviour {
+    private Transform playericon;
+
+	// Use this for initialization
+	void Start () {
+        playericon = MiniMap._instance.GetPlayerIcon();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float y = transform.eulerAngles.y;
+        playericon.localEulerAngles = new Vector3(0, 0, -y);
+	}
+}
