9221f277f93a1c94cf295a42df2dde5f2d051e40
gdmec07131032 <592876487@qq.com>
2015-11-17 22:45:52 +0800

    2015/11/17-22：45

---
+++ Assets/EnemyIcon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyIcon : MonoBehaviour {
+
+    private Transform icon;
+    private Transform player;
+
+    void Start()
+    {
+        if (this.tag == Tags.soulBoss)
+        {
+            icon = MiniMap._instance.GetBossIcon().transform;
+        }
+        else if (this.tag == Tags.soulMonster)
+        {
+            icon = MiniMap._instance.GetMonsterIcon().transform;
+        }
+        player = GameObject.FindGameObjectWithTag(Tags.player).transform;
+
+    }
+
+    void Update()
+    {
+        Vector3 offset = transform.position - player.position;
+        offset *= 10;
+        icon.localPosition = new Vector3(offset.x, offset.z, 0);
+    }
+
+    void OnDestroy()
+    {
+        if (icon != null)
+        {
+            Destroy(icon.gameObject);
+        }
+    }
+}
--- Assets/Script/MiniMap.cs
+++ Assets/Script/MiniMap.cs
@@ -4,19 +4,28 @@ using System.Collections;
 public class MiniMap : MonoBehaviour {
     public static MiniMap _instance;
     private Transform playerIcon;
+    public GameObject enemyIconPrefab;
 
     void Awake()
     {
         _instance = this;
-    }
-
-	// Use this for initialization
-	void Start () {
         playerIcon = transform.Find("PlayerIcon");
-	}
+    }
 
     public Transform GetPlayerIcon()
     {
         return playerIcon;
+    }
+    public GameObject GetBossIcon()
+    {
+        GameObject go = NGUITools.AddChild(this.gameObject, enemyIconPrefab);
+        go.GetComponent<UISprite>().spriteName = "BossIcon";
+        return go;
+    }
+    public GameObject GetMonsterIcon()
+    {
+        GameObject go = NGUITools.AddChild(this.gameObject, enemyIconPrefab);
+        go.GetComponent<UISprite>().spriteName = "EnemyIcon";
+        return go;
     }
 }
